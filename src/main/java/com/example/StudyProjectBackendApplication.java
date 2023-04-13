49a9be0cdf8d8ceb8c6bc488169fa2e4b96b043c
zhangshuaibao922 <zhangshuaibao922@163.com>
2023-04-13 18:58:17 +0800

    创建项目

---
+++ src/main/java/com/example/StudyProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProjectBackendApplication.class, args);
+    }
+
+}
